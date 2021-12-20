b21739277537e064793c7bf9eab11f70330b9e3b
David Wilkins <wilkins.david@hotmail.com>
2021-12-20 22:46:55 +0100

    DMA implemented and tested with test-bench

--- hw/hdl/DMA_sub.vhd
+++ hw/hdl/DMA_sub.vhd
@@ -12,6 +12,7 @@ entity DMA_sub is
 			Wr				: out std_logic;
 			DataWr			: out std_logic_vector(31 downto 0);
 			RdFifo			: out std_logic;
+			Fifo_empty		: in std_logic;
 			RdData			: in std_logic_vector(15 downto 0);
 			FBuff0			: in std_logic_vector(31 downto 0);
 			FBuff1			: in std_logic_vector(31 downto 0);
@@ -20,6 +21,90 @@ entity DMA_sub is
 	end DMA_sub;
 	
 architecture comp of DMA_sub is
+	type State is(S_Init, S_Idle, S_Acq, S_Wait, S_Write, S_Inc, S_BuffInc, S_Hold);
+
+	signal SM			: State 				:= S_Idle;	
+	signal acq			: std_logic 			:= '0';
+	signal pixCount		: unsigned(31 downto 0) := (others => '0');
+	signal buffSel		: std_logic 			:= '0';
+	signal start 		: std_logic				:= '0';
+	signal int_Address	: unsigned(31 downto 0) := (others => '0');
 begin
+	Address <= std_logic_vector(int_Address);
+	process(clk, nReset)
+	begin
+		if nReset = '0' then
+			irq <= '0';
+			Wr <= '0';
+			RdFifo <= '0';
+			DataWr <= (others => '0');
+			SM <= S_Init;
+			acq <= '0';
+			buffSel <= '0';
+			int_Address <= unsigned(FBuff0);
+		elsif rising_edge(clk) then
+			if start_DMA = '1' then
+				start <= '1';
+			end if;
+			case SM is
+				when S_Init =>
+					if start = '1' then
+						SM <= S_Idle;
+					end if;
+					int_Address <= unsigned(FBuff0);
+				when S_Idle =>
+					start <= '0';
+					irq <= '0';
+					if Fifo_empty = '0' then
+						SM <= S_Acq;
+					end if;
+				when S_Acq =>
+					RdFifo <= '1';
+					if acq = '0' then
+						SM <= S_Acq;
+						acq <= '1';
+						DataWr(15 downto 0) <= RdData;
+					else
+						SM <= S_Wait;
+						acq <= '0';
+						DataWr(31 downto 16) <= RdData;
+					end if;
+					
+				when S_Wait =>
+					if WaitRequest = '0' then
+						SM <= S_Write;
+					end if;
+				when S_Write =>
+					SM <= S_Inc;
+					Wr <= '1';
+					
+				when S_Inc =>
+					if pixCount < to_unsigned(76800, pixCount'length) then
+						SM <= S_Idle;
+					else
+						SM <= S_BuffInc;
+					end if;
+					Wr <= '0';
+					int_Address <= int_Address + 4;
+					pixCount <= pixCount + 2;
+				
+				when S_BuffInc =>
+					buffSel <= not buffSel;
+					SM <= S_Hold;
+					
+				when S_Hold =>
+					if buffSel = '0' then
+						int_Address <= unsigned(FBuff0);
+					else
+						int_Address <= unsigned(FBuff1);
+					end if;
+					pixCount <= to_unsigned(0, pixCount'length);		
+					irq <= '1';
+					if start = '1' then
+						SM <= S_Idle;
+					end if;
+			end case;
+		end if;
+	end process;
 
 end comp;
---
+++ hw/hdl/testbenches/DMA_sub_TB.vhd
@@ -0,0 +1,99 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity tb_DMA_sub is
+end tb_DMA_sub;
+
+architecture test of tb_DMA_sub is
+
+		constant CLK_PERIOD : time := 20 ns;		--50 MHz
+		signal sim_finished : boolean := false;
+
+		--input signals
+		signal clk			: std_logic;
+		signal nReset		: std_logic;
+		signal WaitRequest 	: std_logic;
+		signal Fifo_empty	: std_logic;
+		signal RdData		: std_logic_vector(15 downto 0);
+		signal FBuff0		: std_logic_vector(31 downto 0);
+		signal FBuff1		: std_logic_vector(31 downto 0);
+		signal start_DMA	: std_logic;
+		--output signals
+		signal irq			: std_logic;
+		signal Address 		: std_logic_vector(31 downto 0);
+		signal Wr			: std_logic;
+		signal DataWr		: std_logic_vector(31 downto 0);
+		signal RdFifo		: std_logic;
+		
+begin
+
+dut : entity work.DMA_sub
+	port map(
+		clk      			=> clk,   	
+		nReset      		=> nReset,
+		WaitRequest 		=> WaitRequest,
+		Fifo_empty			=> Fifo_empty,
+		irq					=> irq,
+		Address 			=> Address,
+		Wr					=> Wr,
+		DataWr				=> DataWr,
+		RdFifo				=> RdFifo,
+		RdData				=> RdData,
+		FBuff0				=> FBuff0,
+		FBuff1				=> FBuff1,
+		start_DMA			=> start_DMA
+	);
+
+clk_generation : process is
+begin
+	if not sim_finished then
+		clk <= '1';
+		wait for CLK_PERIOD / 2;
+		clk <= '0';
+		wait for CLK_PERIOD / 2;
+	else 
+		wait;
+	end if;
+end process clk_generation;
+
+update_fifo : process(Wr, nReset)
+begin
+	if(nReset = '0') then
+		RdData <= "1100110011001100";
+	else
+		RdData <= std_logic_vector(unsigned(RdData) + 4);
+	end if;
+end process update_fifo;
+  
+simulation : process is
+begin
+	nReset <= '0';
+	wait for CLK_PERIOD;
+	nReset <= '1';
+	WaitRequest <= '1';
+	Fifo_empty <= '1';
+	FBuff0 <= "11111111111111111111111111111111";
+	FBuff1 <= "10101010101010101010101010101010";
+	wait for CLK_PERIOD*4;
+	start_DMA <= '1';
+	wait for CLK_PERIOD;
+	start_DMA <= '0';
+	wait for CLK_PERIOD*4;
+	Fifo_empty <= '0';
+	wait for CLK_PERIOD*2;
+	--RdData <= "0011001100110011";
+	wait for CLK_PERIOD*8;
+	WaitRequest <= '0';
+	wait for 6 ms;
+	--reached
+	wait for CLK_PERIOD*8;
+	start_DMA <= '1';
+	wait for CLK_PERIOD;
+	start_DMA <= '0';
+	wait for 1 ms;
+	sim_finished <= true;
+	wait;	
+end process simulation;
+
+end architecture;
