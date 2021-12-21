2d42efd15859b0b07816f768301fc12935e431fe
David Wilkins <wilkins.david@hotmail.com>
2021-12-21 11:55:23 +0100

    AS with testbench

--- hw/hdl/AS_sub.vhd
+++ hw/hdl/AS_sub.vhd
@@ -15,6 +15,30 @@ entity AS_sub is
 		);
 	end AS_sub;
 	
-architecture comp of AS_sub is	
+architecture comp of AS_sub is
+	constant START_BIT 		: natural := 0;
+	signal int_start		: std_logic := '0';
 begin
+	start <= int_start;
+	process(clk, nReset)
+		begin
+			if nReset = '0' then
+				FBuff0 <= (others => '0');
+				FBuff1 <= (others => '0');
+				int_start <= '0';
+			elsif rising_edge(clk) then
+				if Wr = '1' then
+					case Address is
+						when "00" => FBuff0 <= DataWr;
+						when "01" => FBuff1 <= DataWr;
+						when "10" => int_start <= DataWr(START_BIT);
+						when others => null;
+					end case;
+				end if;
+				--reset start-signal after one clock cycle
+				if int_start = '1' then
+					int_start <= '0'; 
+				end if;
+			end if;
+		end process;
 end comp;
